4b6f7dd1fccacd7c770cb6b9f5d955db54efd3a9
adeshett <adesh.shetty@capgemini.com>
2020-04-17 14:35:40 +0530

    changed settings.py

--- Wallpapers/settings.py
+++ Wallpapers/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.103',
+ALLOWED_HOSTS = ['adeshett.pythonanywhere.com',
                  '127.0.0.1',
                  ]
 
