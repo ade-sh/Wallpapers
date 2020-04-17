c90369b0886429cebcef3b0b784837740b9bb8c1
adeshett <adesh.shetty@capgemini.com>
2020-04-17 14:33:54 +0530

    added secret key

--- Wallpapers/settings.py
+++ Wallpapers/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f)jbq-x7+g*lj=t#0#i6_#(x6dc*83^gvclqvtavx!gs(dx1^='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
